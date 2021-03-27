b1cbd5fdeb0ac68aa4030c72b2e3543e047f3a4d
KScriven <kerryn.scriven@bbc.co.uk>
2021-03-27 14:39:16 +0000

    Initial building of graphQL endpoints for cv

--- src/server.js
+++ src/server.js
@@ -1,20 +1,25 @@
 const ApolloServer = require('apollo-server').ApolloServer
 const ApolloServerLambda = require('apollo-server-lambda').ApolloServer
 const { gql } = require('apollo-server-lambda');
+const contact = require('../assets/contact.json')
 
 const typeDefs = gql`
   type Query {
     hello: String
+    emailAddress: String
+    contactComment: String
   }
 `;
 
 const resolvers = {
   Query: {
-    hello: () => "Hi! With love from Kerryn Scriven"
+    hello: () => "Hi! With love from Kerryn Scriven",
+    emailAddress: () => contact.email_address,
+    contactComment: () => contact.contact_comments
   }
 };
 
-function createLambdaServer () {
+function createLambdaServer() {
   return new ApolloServerLambda({
     typeDefs,
     resolvers,
@@ -23,7 +28,7 @@ function createLambdaServer () {
   });
 }
 
-function createLocalServer () {
+function createLocalServer() {
   return new ApolloServer({
     typeDefs,
     resolvers,
