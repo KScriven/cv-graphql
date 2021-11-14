c4a1e54492b08f7fc902f26bcce900ab9570b3f2
KScriven <kerryn.scriven@bbc.co.uk>
2021-11-14 11:38:50 +0000

    Update to backend and added blog resolver

--- src/server.js
+++ src/server.js
@@ -1,39 +1,67 @@
 const ApolloServer = require('apollo-server').ApolloServer
 const ApolloServerLambda = require('apollo-server-lambda').ApolloServer
 const { gql } = require('apollo-server-lambda');
-const cv = require('./cv.json')
+const cv = require('../src/cv/cv.json')
+const blogs = require('../src/blog/blogs.json')
 
 const typeDefs = gql`
   type Query {
     hello: String 
-    achievements: String
-    currentProgrammingLanguages: String
-    college: String
-    contact: String
-    diploma: String
     emailAddress: String
-    employment: String
-    hobbies: String
-    intro: String
     linkedIn: String
-    technology: String
+    intro: String
+    blogs: [Blog]
+  }
+
+  type Blog {
+    intro: String
+    myBiggestTakeAways: String,
+    theDailyGrind: String,
+    theThingsILove: String,
+    theThingsIDisLike: String,
+    interestingFacts: String
   }
 `;
 
 const resolvers = {
   Query: {
     hello: () => "Created with graphQL by Kerryn Scriven",
-    achievements: () => cv.status,
-    currentProgrammingLanguages: () => cv.currentProgrammingLanguages,
-    college: () => cv.college,
-    contact: () => cv.contact_comments,
-    diploma: () => cv.diploma,
+    // playerStatus: () => player1.playerStatus,
+    // playerMood: String
+    // likes: String
+    // dislikes: String
+    // email_address: String
+    // linkedIn: String
+    // education: String
+    // wouldLikeTo: String
+    // diploma: Boolean
+    // currentJobTitle: String
+    // numberOfPreviousJobs: Int
+    // countriesLivedIn: Int
+    // college: () => cv.college,
+    // contact: () => cv.contact_comments,
+    // diploma: () => cv.diploma,
     emailAddress: () => cv.email_address,
-    employment: () => cv.mainResponsibilities,
-    hobbies: () => cv.hobbies,
+    // employment: () => cv.mainResponsibilities,
+    // hobbies: () => cv.hobbies,
     intro: () => cv.intro,
     linkedIn: () => cv.linkedIn,
-    technology: () => cv.currentProgrammingLanguages,
+    blogs: () => {
+      let blogResults = []
+      blogs.map(blog => {
+        blogResults.push({
+          intro: blog.intro,
+          myBiggestTakeAways: blog.myBiggestTakeAways,
+          theDailyGrind: blog.theDailyGrind,
+          theThingsILove: blog.theThingsILove,
+          theThingsIDisLike: blog.theThingsIDisLike,
+          interestingFacts: blog.interestingFacts
+        })
+      })
+
+      return blogResults
+    }
+    // technology: () => cv.currentProgrammingLanguages,
   }
 };
 
